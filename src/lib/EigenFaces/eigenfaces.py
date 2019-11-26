cb002ede5a730b169b01a1c1eeca09d3eac360fa
Daniel <daniel.bairamian@mail.mcgill.ca>
2019-11-26 00:47:53 -0500

    EigenFace reconstruction done, classification left

--- src/lib/EigenFaces/eigenfaces.py
+++ src/lib/EigenFaces/eigenfaces.py
@@ -38,7 +38,17 @@ def normalize_image(image):
 
     return np.asarray(normalized_img)
 
-def generate_PCA_Dimensionality_Estimation(data):
+def save_top5_eigenfaces(eigenfaces):
+    for i in range (0, 5):
+        plt.figure()
+        eigenfacetemp = normalize_image(eigenfaces[i])
+        eigenfacetemp = eigenfacetemp.reshape(256, 256, 3)
+        plt.imshow(eigenfacetemp)
+        plt.xticks([])
+        plt.yticks([])
+        plt.savefig("EigenFace"+str(i+1)+".png", transparent=True)
+
+def generate_PCA_Dimensionality_Estimation(data, threshold=0.001):
 
     pca = PCA(n_components=data.shape[0])
     pca.fit(data)
@@ -47,7 +57,7 @@ def generate_PCA_Dimensionality_Estimation(data):
     eigenvalue_eigenvector.sort(key=lambda x: x[1], reverse=True)
 
     idx = 0
-    cutoff_threshold = 0.01  # 1% cuttoff threshold
+    cutoff_threshold = threshold  # 0.1% cuttoff threshold
     for vals in eigenvalue_eigenvector:
         idx += 1
         if vals[2] < cutoff_threshold:
@@ -65,17 +75,19 @@ def generate_PCA_Dimensionality_Estimation(data):
     plt.plot(np.linspace(1, data.shape[0], num=data.shape[0]), pca.explained_variance_ratio_)
 
     plt.plot(np.linspace(1, data.shape[0], num=data.shape[0]),
-             np.linspace(cutoff_threshold, cutoff_threshold, num=data.shape[0]))
+             np.linspace(cutoff_threshold, cutoff_threshold, num=data.shape[0]), alpha=0.65, color='orange')
     plt.scatter(idx, cutoff_threshold, color='red')
-    plt.text(idx, cutoff_threshold + 0.025,
+    plt.text(int(data.shape[0]/2), cutoff_threshold + 0.025,
              s="PC# " + str(idx) + " | Cuttoff Threshold: " + str(cutoff_threshold * 100) + "%")
     plt.savefig('Individual-Variance.png')
+    plt.close()
 
     return idx
 
 
 if __name__ == "__main__":
 
+    # load data
     data = data_loader.get_data(isCropped=False)
 
     X_train = data[0]
@@ -91,20 +103,70 @@ if __name__ == "__main__":
     X_train_flat = flatten_and_gscale(X_train)
     X_test_flat  = flatten_and_gscale(X_test)
 
+    # get a dimensionality estimation
     optimal_dims = generate_PCA_Dimensionality_Estimation(X_train_flat)
 
-   # pca = PCA(n_components=optimal_dims)
-    #pca.fit(data)
+    # run PCA with the optimal number of dimensions
+    pca = PCA(n_components=optimal_dims)
+    # fit our data
+    pca.fit(X_train_flat)
 
-    #eigenvalue_eigenvector = list(zip(pca.components_, pca.explained_variance_, pca.explained_variance_ratio_))
-    #eigenvalue_eigenvector.sort(key=lambda x: x[1], reverse=True)
+    # sort eigenvectors by eigenvalue
+    eigenvalue_eigenvector = list(zip(pca.components_, pca.explained_variance_, pca.explained_variance_ratio_))
+    eigenvalue_eigenvector.sort(key=lambda x: x[1], reverse=True)
 
-    # print(eigenspaceData.shape)
-    # print(np.shape(eigenspaceData))
+    # separate them
+    eigenvectors = np.asarray([x[0] for x in eigenvalue_eigenvector])
+    eigenvalues  = np.asarray([x[1] for x in eigenvalue_eigenvector])
+
+    # save the top 5 eigen faces
+    save_top5_eigenfaces(eigenvectors)
+
+    # get the mean face
+    mean_face = pca.mean_
+
+
+    # testing eigenface reconstruction with one image
+    eigenImages = pca.transform(X_test_flat)
+    firstimage = eigenImages[0]
+    testimg = X_test_flat[0] - mean_face
+    testimg = np.dot(testimg.T, eigenvectors.T)
+
+    reconstruction = 0
+    for eigenF, eigenW in zip(eigenvectors, testimg):
+        reconstruction += eigenF*eigenW
+    reconstruction = mean_face + reconstruction
+
+    reconstruction = normalize_image(reconstruction)
+    reconstruction = reconstruction.reshape(256, 256, 3)
+
+    cv2.imshow("init", X_test[0])
+    cv2.imshow("reconstruction", reconstruction)
+    cv2.waitKey(0)
+
+
+
+
+    #testimg = X_train_flat[0] - mean_face
+    #testimg = np.dot(testimg.T, eigenvectors.T)
+
+    #testimg = testimg.reshape(32, 32)
+   #plt.imshow(testimg)
+   # plt.show()
+    #print(testimg.shape)
+    #testimg = normalize_image(testimg)
+    #plt.imshow(testimg)
+    #plt.show()
+
+    # print(np.shape(X_test_flat))
+    # print(eigenvectors.shape)
+    # print(eigenvalues.shape)
+    # print(mean_face.shape)
+    # print(eigenvalue_eigenvector[0][0].shape)
+    #
     # print(np.shape(pca.mean_))
     # print(np.shape(pca.components_))
     # print(np.shape(pca.explained_variance_))
-    # print(eigenVecs)
 
 
 
