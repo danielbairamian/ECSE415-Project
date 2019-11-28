3bfbb0ff1fc0f375b6886c78f40f778388fa88e9
leakkari <lea.akkary@gmail.com>
2019-11-28 11:05:58 -0500

    Hog Features Added

--- src/helpers/hog.py
+++ src/helpers/hog.py
@@ -1,15 +1,16 @@
 from skimage import feature as ft
 import cv2
 
-def hog_ft(imgs, bins=9, blocks=(8,8), cells=(2,2)):
-    '''
-    Returns hog feats array
-    '''
-    hog_feats = []
-    for i in range(len(imgs)):
-        image = imgs[i]
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        x = ft.hog(gray, orientations=bins, pixels_per_cell=blocks,
-                   cells_per_block=cells, block_norm='L2-Hys')
-        hog_feats.append(x)
-    return hog_feats
+# ----- HoG Function ----- #
+def rgb2gray(im):
+    gray = im[:, :, 0] * 0.2989 + im[:, :, 1] * 0.5870 + im[:, :, 2] * 0.1140
+    return gray
+
+def hog_features(imgs, cells):
+    train_x = []
+    for data in imgs:
+        gray = rgb2gray(data) / 255.0
+        x = ft.hog(gray, orientations=9, pixels_per_cell=(2, 2),
+                   cells_per_block=(cells, cells))
+        train_x.append(x)
+    return train_x
--- src/lib/SIFT/sift_sizes.py
+++ src/lib/SIFT/sift_sizes.py
@@ -2,6 +2,7 @@ from src.helpers import data_loader
 from src.helpers import hog
 from src.helpers import patch_maker
 import matplotlib.pyplot as plt
+from src.helpers import hog
 
 import cv2
 
@@ -57,16 +58,29 @@ if __name__ == '__main__':
     metadata = data[4]
 
     # Display Cropped and Original Image
-    plt.imshow(X_train_cropped[0])
+    plt.imshow(X_train_cropped[1])
     plt.show()
-    plt.imshow(X_train[0])
+    plt.imshow(X_train[1])
     plt.show()
 
-    cropped_img = X_train_cropped[0]
-    original_img = X_train[0]
+    cropped_img = X_train_cropped[1]
+    original_img = X_train[1]
 
     # Get Sift Keypoints coordinates on cropped image
     kp = sift(cropped_img)
 
     # Patch image
     patches = patch_maker.make_patch(img=original_img, img_cropped=cropped_img, size=15, k=kp)
+
+    '''
+    Extract HoG descriptor for the patch as follows.
+    Fix the block size= 2, number of bins = 9 and vary the cell size. 
+    Use cell sizes 3 × 3, 4 × 4 and 5 × 5 . 
+    Build separate vocabularies for HoG descriptor with different cell sizes. 
+    Test and evaluate the vocabularies using recognition rate. 
+    Plot recognition rate (on y-axis) vs cell size of the HoG descriptor (on x-axis). 
+    Compute confusion matrix for the best performing vocabulary.
+    '''
+    hog_3 = hog.hog_features(patches, 3)
+    hog_4 = hog.hog_features(patches, 4)
+    hog_5 = hog.hog_features(patches, 5)
