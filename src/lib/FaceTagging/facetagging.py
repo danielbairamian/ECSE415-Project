ce3816eb8adfccbc1dc6d96dc4f6dbd023730b71
Daniel <daniel.bairamian@mail.mcgill.ca>
2019-12-01 13:34:21 -0500

    FaceTagging with random group photos

--- src/lib/FaceTagging/facetagging.py
+++ src/lib/FaceTagging/facetagging.py
@@ -8,9 +8,52 @@ import copy
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.metrics import accuracy_score, confusion_matrix
-
+from src.helpers.TemplateMatching import get_coordinates
 
 from src.lib.EigenFaces import eigenfaces
+
+
+def tag_group_pictures(group_photo, cropped_photos, labels):
+    # save all coordinates
+    coords_list = []
+    for cp in cropped_photos:
+        coords = get_coordinates(group_photo, cp)
+        coords = [coords[0], coords[1], cp.shape[0], cp.shape[1]]
+        coords_list.append(coords)
+
+    # make a copy of our group photo
+    temp_img_final = copy.deepcopy(group_photo)
+    # for each coordinate we have
+    # place the bounding box and tag the image
+    for i, coord in enumerate(coords_list):
+        x = coord[0]
+        y = coord[1]
+
+        w = coord[2]
+        h = coord[3]
+
+        # bounding box variables
+        start_point = (x, y)
+        end_point   = (x+w, y+h)
+
+        color = (255, 0, 0)
+        thickness = 1
+
+        # text variables
+        font = cv2.FONT_HERSHEY_PLAIN
+        bottomLeftCornerOfText = start_point
+        fontScale = ((w+h))*2 / group_photo.shape[0]
+        fontColor = (0, 0, 0)
+        lineType = 1
+
+        # put the text and the bounding box on the image
+        tempImg = cv2.rectangle(group_photo, start_point, end_point, color, thickness)
+        cv2.putText(tempImg, labels[i], bottomLeftCornerOfText, font,fontScale, fontColor, lineType)
+
+        temp_img_final = copy.deepcopy(tempImg)
+
+    # return the final tagged image
+    return temp_img_final
 
 def FaceTagging(datacropped=True):
     All_Groups = data_loader.get_group_data()
@@ -41,20 +84,45 @@ def FaceTagging(datacropped=True):
     # transform all training images to eigen space
     eigenImagesTrain = pca.transform(X_train_flat)
 
-    # load our group data
-    testbatch = []
-    for i in range (1, 6):
-        imgtemp = All_Groups[0][i]
-        imgtemp = cv2.resize(imgtemp, (imgshape, imgshape))
-        testbatch.append(imgtemp)
-        print(np.shape(imgtemp))
-    print(np.shape(testbatch))
-    testbatch = np.asarray(testbatch)
-    testbatch = eigenfaces.flatten_and_gscale(testbatch)
 
-    eigentest = pca.transform(testbatch)
-    ypred = eigenfaces.classify(eigenImagesTrain, Y_train, eigentest)
-    print(ypred)
+    final_tagged_images = []
+
+    # for all groups that we have
+    for group in All_Groups:
+        # the first image is the group picture
+        original_group = group[0]
+        cropped_group  = []
+        cropped_group_resized = []
+        # we have 5 trained individuals, so for 1->5 (0 is the group)
+        for i in range (1, 6):
+            # save the image
+            temp_img = group[i]
+            # save original sized image
+            cropped_group.append(copy.deepcopy(temp_img))
+            # save a resized image to fit to PCA
+            cropped_group_resized.append(cv2.resize(copy.deepcopy(temp_img), (imgshape, imgshape)))
+
+        # flatten and project resized faces to eigenspace
+        cropped_group_resized = np.asarray(cropped_group_resized)
+        cropped_group_resized = eigenfaces.flatten_and_gscale(cropped_group_resized)
+        cropped_group_resized = pca.transform(cropped_group_resized)
+
+        # run our classifier to get prediction labels
+        ypred_temp = eigenfaces.classify(eigenImagesTrain, Y_train, cropped_group_resized)
+        # get the name associated to the labels
+        labels_tags = [metadata[x] for x in ypred_temp]
+
+        # helper function that will place the name and the box around the group image
+        final_tagged_images.append(tag_group_pictures(original_group, cropped_group, labels_tags))
+
+    for i, taggedIm in enumerate(final_tagged_images):
+        plt.figure()
+        plt.imshow(taggedIm)
+        plt.xticks([])
+        plt.yticks([])
+        plt.savefig("GroupImgs/Group"+str(i+1)+".png", transparent=True)
+        plt.close()
+
 
 if __name__ == "__main__":
     FaceTagging(True)
